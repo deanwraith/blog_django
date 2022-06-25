b6e22a7d6e45b565e790ca196db24cc9966f6d93
deanwraith <dean.wraith@gmail.com>
2022-06-25 10:45:14 +0000

    Created Post View

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title','status','slug','author','publish')
+
+admin.site.register(models.Post, AuthorAdmin)
