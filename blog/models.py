d2cfa829d5a05c89fc3ce03eda565d130aba2091
deanwraith <dean.wraith@gmail.com>
2022-06-25 09:38:28 +0000

    Created Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.urls import reverse
 
 # Create your models here.
+
+class Post (models.Model):
+
+    class NewManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    options = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    publish = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey (User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = CloudinaryField('image', default='placeholder')
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    objects = models.Manager() #default manager
+    newmanager = NewManager() #custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_single',args=[self.slug])
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
